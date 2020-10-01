9367a7a868bbb273653f8cefe0bcd0a4a2d8992c
Arpit Kumar Jain <46723473+arpitkekri@users.noreply.github.com>
2020-10-02 02:36:41 +0530

    Update arrays.cpp

--- Arrays and Pointers/arrays.cpp
+++ Arrays and Pointers/arrays.cpp
@@ -1,18 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <climits>
-#include <algorithm>
-#include <cmath>
-#include <bitset>
-#include <cstdio>
-#include <cstring>
-#include <stack>
-#include <iomanip>
-#include <set>
-#include <map>
-// #include<bits/stdc++.h>  // By adding this single header file we can add all standard header files
+#include<bits/stdc++.h>
 using namespace std;
 
 void swap(int arr[], int i, int j) {
